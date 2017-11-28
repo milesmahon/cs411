2814056690d8efa2814d885bedf58d35e6c6aab4
j-silverman <jhs2018@bu.edu>
2017-11-27 20:11:57 -0500

    added basic socket chat implementation from miguelgrinberg's Flask-SocketIO example

--- Sync/app.py
+++ Sync/app.py
@@ -75,7 +75,6 @@ def logout():
 
 @app.route('/info')
 def get_info():
-    print(ACCESS_TOKEN)
     access_token = ACCESS_TOKEN[str(current_user.id)]
     print("access token = " + access_token)
     auth_header = {"Authorization":"Bearer {}".format(ACCESS_TOKEN[str(current_user.id)])}
@@ -123,9 +122,21 @@ def oauth_callback(provider):
     ACCESS_TOKEN[str(current_user.id)] = access_token
     return redirect(url_for('index'))
 
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
 @socketio.on('connect', namespace='/test')
 def test_connect():
     emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
 
 if __name__ == "__main__":
